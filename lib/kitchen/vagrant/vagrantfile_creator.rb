08b52ba139374b0233f5a2b3b85736eb97a5e736
Mark Borcherding <MarkBorcherding@gmail.com>
2013-08-08 16:05:51 -0500

    Update vagrantfile_creator.rb
    Use default host instead of just host for SSH config

--- lib/kitchen/vagrant/vagrantfile_creator.rb
+++ lib/kitchen/vagrant/vagrantfile_creator.rb
@@ -53,7 +53,7 @@ module Kitchen
         arr << %{  c.vm.box_url = "#{config[:box_url]}"} if config[:box_url]
         arr << %{  c.vm.hostname = "#{instance.name}.vagrantup.com"}
         arr << %{  c.ssh.username = "#{config[:username]}"} if config[:username]
-        arr << %{  c.ssh.host = "#{config[:ssh_host]}"} if config[:ssh_host]
+        arr << %{  c.ssh.default.host = "#{config[:ssh_host]}"} if config[:ssh_host]
       end
 
       def guest_block(arr)
